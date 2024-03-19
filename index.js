17285595b70358838f4649126e108a8d4d7851cb
Kundan Baidhya <baidyapradip005@gmail.com>
2024-03-19 10:47:50 +0545

    Final push fixes all errors and code

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const excelToJson = require('convert-excel-to-json');
-const prompt = require('prompt-sync')();
-const fs = require('fs');
-const {myFunc} = require('./Helper');
+const excelToJson = require("convert-excel-to-json");
+const prompt = require("prompt-sync")();
+const fs = require("fs");
+const { myFunc } = require("./helper");
 
-const result = excelToJson({sourceFile: 'Presidents.xlsx'});
+const result = excelToJson({ sourceFile: "Presidents.xlsx" });
 
 console.log(result); //log to show the result that has been parsed from the excel file
 
-
-const language = prompt("Enter which language do you want your file to be : English Spanish Chinese = ");
+const language = prompt(
+  "Enter which language do you want your file to be : English Spanish Chinese = "
+);
 //Shows a log and takes input and stores it in a language constant
 
-const mergedObject = {};// Initialize an empty object for merging
+const mergedObject = {}; // Initialize an empty object for merging
 
-switch (language) {//switch case for different language input
-    case 'English':
-            myFunc('B', 'English');
-            break;
+//switch case for different language input
+switch (language) {
+  case "English":
+    myFunc("B", "English");
+    break;
 
-    case 'Spanish':
-            myFunc('C', 'Spanish');
-        break;
+  case "Spanish":
+    myFunc("C", "Spanish");
+    break;
 
-    case 'Chinese':
-            myFunc('D', 'Chinese')
-        break;
+  case "Chinese":
+    myFunc("D", "Chinese");
+    break;
 
-    default:
-        console.log('Please choose a given languages only.');
-        break;
+  default:
+    console.log("Please choose a given languages only.");
+    break;
 }
