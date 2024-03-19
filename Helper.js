b13fe28cf0b144d00f60d1e5d58630431e592343
bibash16 <bibhashtamrakar@gmail.com>
2024-03-19 15:19:18 +0545

    Add: allows user to choose file type after selecting language and convert to that filetype

--- Helper.js
+++ Helper.js
@@ -2,12 +2,12 @@ const excelToJson = require('convert-excel-to-json');
 const prompt = require('prompt-sync')();
 const fs = require('fs');
 
-const result = excelToJson({sourceFile: 'Presidents.xlsx'});
+const result = excelToJson({ sourceFile: 'Presidents.xlsx' });
 
 const mergedObject = {};
 const column = '';
 
-const myFunc = (column, Language)=>{
+const myFunc = (column, Language, LanType) => {
     result.sharingModal2.forEach(element => {
         const str1 = element.A.toString();
         const str2 = String(element[column]);
@@ -16,21 +16,33 @@ const myFunc = (column, Language)=>{
         let currentObj = mergedObject;
 
         keys.forEach((key, index) => {
-            if (index === keys.length - 1) { 
+            if (index === keys.length - 1) {
                 // Last key, assign the value
                 currentObj[key] = str2;
             } else {
                 currentObj[key] = currentObj[key] || {};
                 currentObj = currentObj[key];
             }
-            });
         });
+    });
 
-        // Writing the merged object to a file
-        fs.writeFileSync(Language+'.json', JSON.stringify(mergedObject, null, 2));
-
-        console.log('JSON written to '+Language+'.json');
-
+    // Writing the merged object to a file
+    if (LanType === 'json') {
+        // INTO JSON
+        fs.writeFileSync(Language + '.json', JSON.stringify(mergedObject, null, 2));
+        console.log('JSON written to ' + Language + '.json');
+    } else if (LanType === 'js') {
+        // INTO JS
+        fs.writeFileSync(Language + '.js', `module.exports = ${JSON.stringify(mergedObject, null, 2)};`);
+        console.log('JavaScript written to ' + Language + '.js');
+    } else if (LanType === 'ts') {
+        //  INTO TS
+        // importing data from the file would require using the "data" constant
+        fs.writeFileSync(Language + '.ts', `export const data = ${JSON.stringify(mergedObject, null, 2)};`);
+        console.log('TypeScript written to ' + Language + '.ts');
+    } else {
+        console.log("Invalid file type! Please enter either: JSON, JS or TS.")
+    };
 }
 
-module.exports = {myFunc,};
+module.exports = { myFunc, };
